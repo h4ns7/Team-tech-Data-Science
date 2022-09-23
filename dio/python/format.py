83aa77eed19f954ba6cca322aaf9c225287bc90d
h4ns7 <muller97_@outlook.com>
2022-09-23 15:27:11 -0300

    formatação de print .format

---
+++ dio/python/format.py
@@ -0,0 +1,7 @@
+nome = "Diego"
+idade = 24
+profissao = "Cientista de dados"
+nascionalidade = "Brasileiro"
+
+print(
+    f'Meu nome é {nome} e tenho {idade} onde sou  {profissao} e minha nascionalidade é {nascionalidade}.')
